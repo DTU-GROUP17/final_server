23a9ff0dc040cb1e422caed7eaf91b29777cf81b
Oliver Nybroe <olivernybroe@gmail.com>
2017-04-04 14:41:49 +0200

    added migration file.

--- src/models/User.java
+++ src/models/User.java
@@ -1,11 +1,19 @@
 package models;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "user")
 public class User extends Model {
 
+	@Id
+	@GeneratedValue
+	@Column(nullable = false, unique = true)
+	private int id;
+
 	private ArrayList<Role> roles;
-	
+
 	private String name;
 
 }
