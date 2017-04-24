1719f57abdda34747e7f6ae8d0ba2ace6828de93
Oliver Nybroe <olivernybroe@gmail.com>
2017-04-24 18:55:52 +0200

    Fixed issue with wrong parameters.

--- src/http/requests/LoginInfo.java
+++ src/http/requests/LoginInfo.java
@@ -1,7 +1,9 @@
 package http.requests;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginInfo {
 	@Getter private String userName;
 	@Getter private String password;
