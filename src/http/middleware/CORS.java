7b42c924a887153c227dab36645031c86c9eb2fa
Oliver Nybroe <olivernybroe@gmail.com>
2017-04-25 09:05:21 +0200

    Added CORS middleware

--- src/http/middleware/CORS.java
+++ src/http/middleware/CORS.java
@@ -5,17 +5,19 @@ import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Provider
 @Priority(Priorities.HEADER_DECORATOR)
-public class ContentType implements ContainerResponseFilter {
+public class CORS implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-		if(!responseContext.getHeaders().containsKey("Content-Type")) {
-			responseContext.getHeaders().add("Content-Type", MediaType.APPLICATION_JSON);
-		}
+		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
+		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
 	}
 }
