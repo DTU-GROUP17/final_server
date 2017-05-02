1759d6f61ec185dfec5046653645c5fe3a1d5666
guldfisk <ce.guldfisk@gmail.com>
2017-05-02 16:14:14 +0200

    wauw

--- src/http/controllers/users/SelfController.java
+++ src/http/controllers/users/SelfController.java
@@ -2,8 +2,15 @@ package http.controllers.users;
 
 import annotations.http.Authenticated;
 import annotations.http.PATCH;
+import app.App;
+import http.requests.CreateUserInfo;
+import models.Role;
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import services.authentication.Guard;
 
+import javax.persistence.PersistenceException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,7 +29,24 @@ public class SelfController {
 	}
 
 	@PATCH
-	public Response update() {
-		return Response.noContent().build();
+	public Response update(@Context Guard guard, CreateUserInfo info) {
+		try (Session session = App.factory.openSession()) {
+			Transaction transaction = session.beginTransaction();
+			User user = (User) guard.getUser();
+			if (info.getName()!=null)
+				user.setName(info.getName());
+			if (info.getUserName()!=null)
+				user.setUserName(info.getUserName());
+			if (info.getPassword()!=null)
+				user.setPassword(info.getPassword());
+			if (info.getRoles()!=null){
+				user.setRoles(Role.getRolesFomNames(session, info.getRoles()));
+			}
+			session.persist(user);
+			transaction.commit();
+			return Response.ok().build();
+		} catch (PersistenceException e) {
+			return Response.notModified().build();
+		}
 	}
 }
