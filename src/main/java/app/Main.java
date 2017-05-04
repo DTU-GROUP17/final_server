b167b9d1e26c97feece79cdf287ab249a47b51f6
Oliver Nybroe <olivernybroe@gmail.com>
2017-05-04 14:03:58 +0200

    okay wahtever

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class Main {
-	public static String port = "8080";
+	public static String port = "9998";
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		App.initHibernate();
