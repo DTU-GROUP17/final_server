e7bdb8ff9c78843225c92cc2be48c559ed27d195
Ali.Ghodrat <ght.ali@hotmail.com>
2017-06-06 00:10:12 +0200

    Added some data into weights sql for test

---
+++ src/main/resources/database/seeds/V1_10__seed_weights.sql
@@ -0,0 +1,2 @@
+INSERT INTO weights (name, uri, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by) VALUES
+  ('Salt', '192.168.0.1', '2017-05-05 03:14:07','Lene erekt','2017-05-05 04:14:07','hax','','');
