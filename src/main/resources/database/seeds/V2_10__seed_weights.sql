bcc69bf8760d3e567bbb5f3d7b0813a4933d6720
Ali.Ghodrat <ght.ali@hotmail.com>
2017-06-06 09:18:13 +0200

    Weights corrected

--- src/main/resources/database/seeds/V2_10__seed_weights.sql
+++ src/main/resources/database/seeds/V2_10__seed_weights.sql
@@ -1,2 +1,2 @@
-INSERT INTO weights (name, uri, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by) VALUES
-  ('Salt', '192.168.0.1', '2012-09-17','1','','','','');
+INSERT INTO weights (name, uri, created_at, created_by, updated_at, updated_by) VALUES
+  ('Salt', '192.168.0.1', CURRENT_TIMESTAMP(), 1 ,CURRENT_TIMESTAMP(), 1,);
