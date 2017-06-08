1caf185b97895dca649b264ea9611752501c1ae9
dukocuk <duran.kse@gmail.com>
2017-06-08 15:37:16 +0200

    added new user

--- src/main/resources/database/seeds/V2_3__seed_users.sql
+++ src/main/resources/database/seeds/V2_3__seed_users.sql
@@ -2,7 +2,9 @@ INSERT INTO users (name, username, password) VALUES
   ('Iohan', 'strazz', ':)'),
   ('Hans', 'hax', '$2a$10$0LTM2R3b6gDcE6BNfe.6D.U5z9h2XFFVkcBuGVgBtQxTXkk7Scz4W'),
   ('Lene', 'erekt', 'q'),
-  ('Lone', 'pew', 'e');
+  ('Lone', 'pew', 'e')
+  ('Farmeceut','farm','$2a$06$G6HyWXDMsIIaqQ67xEx6W.Jj/TliNrBFcjQd4t50lxPWMlpXibOQa')
 
 INSERT INTO users_roles (user_id, role_id) VALUES
   (2, 1);
+  (2, 2);
